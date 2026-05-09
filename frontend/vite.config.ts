e22afbc4db4a677f34b0a2b337997e9789af6432
Mridul Verma <MridulVerma1@Mriduls-MacBook-Air.local>
2026-05-09 15:14:25 +0530

    fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+export default defineConfig({
+  plugins: [react()],
 
-  export default defineConfig({
-    
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      target: 'esnext',
-      outDir: 'build',
-    },
-    server: {
-      host: '0.0.0.0',
-      allowedHosts: [
-        'finite-paced-identify.ngrok-free.dev',
-      ],
-      port: 3000,
-      open: true,
-    },
-  });
+  },
+
+  build: {
+    target: 'esnext',
+    outDir: 'build',
+    sourcemap: false,
+  },
+});
